3580a83a7dad9337941695027cd2b2131d692aa4
Simon Martinelli <sm@simas.ch>
2013-05-12 22:00:56 +0200

    wip

--- jtaf-web/web/javascript/navigation.js
+++ jtaf-web/web/javascript/navigation.js
@@ -1,3 +1,18 @@
 xhrGet("/jtaf/navigation.html", function(response) {
-    document.getElementById("navigation").innerHTML = response;
+    var table = document.createElement("table");
+    table.setAttribute("style", "width: 100%;");
+    var tr = document.createElement("tr");
+    table.appendChild(tr);
+    var td = document.createElement("td");
+    td.innerHTML = response;
+    tr.appendChild(td);
+    var competition = JSON.parse(localStorage.getItem("competition"));
+    if (competition !== undefined) {
+        td = document.createElement("td");
+        td.setAttribute("style", "text-align: right;");
+        td.innerHTML = competition.name;
+        tr.appendChild(td);
+    }
+    document.getElementById("navigation").appendChild(table);
 });
+
