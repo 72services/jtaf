336c4f080e855df6cb2de76bc8bc4ccbc446012b
Simon Martinelli <sm@simas.ch>
2013-05-24 20:57:53 +0200

    wip

--- jtaf-web/web/footer.js
+++ jtaf-web/web/footer.js
@@ -1,4 +1,4 @@
 var footer = document.createElement("div");
 footer.id = "footer";
-footer.innerHTML = "&copy; 2013 by Simon Martinelli - <a href='https://github.com/simasch/jtaf' target='_blank'>JTAF on GitHub</a> - Version 0.3";
+footer.innerHTML = "&copy; 2013 by Simon Martinelli - <a href='https://github.com/simasch/jtaf' target='_blank'>JTAF on GitHub</a> - Version 0.4";
 document.body.appendChild(footer);
