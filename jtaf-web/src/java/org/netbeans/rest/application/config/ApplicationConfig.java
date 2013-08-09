95e7226e4e5acd94ec41081189d288426ce22fb6
Simon Martinelli <sm@simas.ch>
2013-08-09 12:38:27 +0200

    wip

--- jtaf-web/src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ jtaf-web/src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -31,6 +31,8 @@ public class ApplicationConfig extends Application {
         // Following code can be used to customize Jersey JSON provider:
         // Following code can be used to customize Jersey JSON provider:
         // Following code can be used to customize Jersey JSON provider:
+        // Following code can be used to customize Jersey JSON provider:
+        // Following code can be used to customize Jersey JSON provider:
         return resources;
     }
 }
