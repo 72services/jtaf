7f1ec2ead452629646dc1222dc21b9b1f798132b
Simon Martinelli <sm@simas.ch>
2014-04-14 17:29:33 +0200

    change copyright notice

--- jtaf-web/src/main/webapp/footer.js
+++ jtaf-web/src/main/webapp/footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
     var footer = document.createElement("div");
     footer.id = "footer";
-    footer.innerHTML = "&copy; 2013 by Simon Martinelli - "
+    footer.innerHTML = "&copy; 2014 by Simon Martinelli - "
             + "<a href='https://github.com/simasch/jtaf' target='_blank' style='color: white;   '>JTAF on GitHub</a>"
             + " - Version ${project.version}";
     document.body.appendChild(footer);
