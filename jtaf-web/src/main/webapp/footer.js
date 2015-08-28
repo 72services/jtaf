f906fe7b4402e134ee03153738850470c5871ba3
Simon Martinelli <sm@simas.ch>
2015-08-28 13:40:30 +0200

    fixed version

--- jtaf-web/src/main/webapp/footer.js
+++ jtaf-web/src/main/webapp/footer.js
@@ -2,8 +2,8 @@ function Footer() {
 
     var footer = document.createElement("div");
     footer.id = "footer";
-    footer.innerHTML = "&copy; 2014 by Simon Martinelli - "
-            + "<a href='https://github.com/simasch/jtaf' target='_blank' style='color: white;   '>JTAF on GitHub</a>"
+    footer.innerHTML = "&copy; 2015 by Simon Martinelli, <a href='http://www.simas.ch' target='_blank' style='color: white;'>simas GmbH</a> - "
+            + "<a href='https://github.com/simasch/jtaf' target='_blank' style='color: white;'>JTAF on GitHub</a>"
             + " - Version ${project.version}";
     document.body.appendChild(footer);
 
