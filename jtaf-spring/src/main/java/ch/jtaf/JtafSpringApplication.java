4a53778382407722e1c6313cf846c8ddaa76c5fc
Simon Martinelli <sm@simas.ch>
2017-09-26 07:16:13 +0200

    added spring app and removed android app

---
+++ jtaf-spring/src/main/java/ch/jtaf/JtafSpringApplication.java
@@ -0,0 +1,12 @@
+package ch.jtaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtafSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtafSpringApplication.class, args);
+	}
+}
