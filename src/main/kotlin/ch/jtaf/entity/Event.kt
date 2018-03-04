b1ac013bb33e22f6922dc00a4349cf5bc1753951
Simon Martinelli <simon@72.services>
2018-03-04 20:33:38 +0100

    added result calculation and removed athlete edit from results page

--- src/main/kotlin/ch/jtaf/entity/Event.kt
+++ src/main/kotlin/ch/jtaf/entity/Event.kt
@@ -22,4 +22,30 @@ data class Event(
         var position: Int = 0,
 
         var organizationId: Long? = null
-)
+) {
+
+    fun calculatePoints(result: String): Long {
+        var points = 0.0
+        if (result.toDouble() > 0) {
+            when (eventType) {
+                EventType.RUN -> {
+                    points = a * Math.pow((b - result.toDouble() * 100) / 100, c)
+                }
+                EventType.RUN_LONG -> {
+                    val parts = result.split("\\.".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
+                    var time = 0.0
+                    time = if (parts.size == 1) {
+                        parts[0].toDouble() * 60
+                    } else {
+                        parts[0].toDouble() * 60 + parts[1].toDouble()
+                    }
+                    points = a * Math.pow((b - time * 100) / 100, c)
+                }
+                EventType.JUMP_THROW -> {
+                    points = a * Math.pow((result.toDouble() * 100 - b) / 100, c)
+                }
+            }
+        }
+        return Math.round(points)
+    }
+}
