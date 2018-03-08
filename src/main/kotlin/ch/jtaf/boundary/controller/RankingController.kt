6e6760681211f835f328ab27f8c7729ca5160670
simasch <simon@72.services>
2018-03-08 14:51:45 +0100

    first version of pdf competition ranking

--- src/main/kotlin/ch/jtaf/boundary/controller/RankingController.kt
+++ src/main/kotlin/ch/jtaf/boundary/controller/RankingController.kt
@@ -1,16 +1,30 @@
 package ch.jtaf.boundary.controller
 
 import ch.jtaf.control.service.CompetitionRankingService
+import org.springframework.http.MediaType
+import org.springframework.http.MediaType.*
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpHeaders
+
 
 @Controller
 class RankingController(private val rankingService: CompetitionRankingService) {
 
-    @GetMapping("/ranking/competition/{id}")
-    fun get(@PathVariable("id") competitionId: Long): ByteArray{
-        return rankingService.createCompetitionRanking(competitionId)
+    @GetMapping("/ranking/competition/{id}", produces = arrayOf(APPLICATION_PDF_VALUE))
+    fun get(@PathVariable("id") competitionId: Long): ResponseEntity<ByteArray> {
+        val competitionRanking = rankingService.createCompetitionRanking(competitionId)
+
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.parseMediaType("application/pdf")
+        val filename = "competition_$competitionId.pdf"
+        headers.setContentDispositionFormData(filename, filename)
+        headers.cacheControl = "must-revalidate, post-check=0, pre-check=0"
+
+        return ResponseEntity(competitionRanking, headers, HttpStatus.OK)
     }
 
 }
