fae0f65f1cff6d1460663dad547cf3a0649fdf54
simasch <simon@72.services>
2018-03-19 16:58:41 +0100

    parameter name refactoring

--- src/main/kotlin/ch/jtaf/boundary/controller/RankingController.kt
+++ src/main/kotlin/ch/jtaf/boundary/controller/RankingController.kt
@@ -19,46 +19,46 @@ class RankingController(private val competitionRankingService: CompetitionRankin
 
     val httpContentUtil = HttpContentProducer()
 
-    @GetMapping("/ranking/competition/{id}")
-    fun getCompetitionRanking(@PathVariable("id") competitionId: Long): ModelAndView {
+    @GetMapping("/ranking/competition/{competitionId}")
+    fun getCompetitionRanking(@PathVariable("competitionId") competitionId: Long): ModelAndView {
         val mav = ModelAndView("/competition_ranking")
         mav.model["data"] = competitionRankingService.getCompetitionRankingData(competitionId)
         return mav
     }
 
-    @GetMapping("/ranking/competition/{id}/pdf", produces = [APPLICATION_PDF_VALUE])
-    fun getCompetitionRankingAsPdf(@PathVariable("id") competitionId: Long): ResponseEntity<ByteArray> {
+    @GetMapping("/ranking/competition/{competitionId}/pdf", produces = [APPLICATION_PDF_VALUE])
+    fun getCompetitionRankingAsPdf(@PathVariable("competitionId") competitionId: Long): ResponseEntity<ByteArray> {
         val competitionRanking = competitionRankingService.createCompetitionRanking(competitionId)
         return httpContentUtil.getContentAsPdf("competition_$competitionId.pdf", competitionRanking)
     }
 
-    @GetMapping("/ranking/competition/{id}/events", produces = [APPLICATION_PDF_VALUE])
-    fun getEventRanking(@PathVariable("id") competitionId: Long): ResponseEntity<ByteArray> {
+    @GetMapping("/ranking/competition/{competitionId}/events", produces = [APPLICATION_PDF_VALUE])
+    fun getEventRanking(@PathVariable("competitionId") competitionId: Long): ResponseEntity<ByteArray> {
         val eventRanking = competitionRankingService.createEventsRanking(competitionId)
         return httpContentUtil.getContentAsPdf("eventsranking_$competitionId.pdf", eventRanking)
     }
 
-    @GetMapping("/ranking/competition/{id}/diplomas", produces = [APPLICATION_PDF_VALUE])
-    fun getDiplomas(@PathVariable("id") competitionId: Long): ResponseEntity<ByteArray> {
+    @GetMapping("/ranking/competition/{competitionId}/diplomas", produces = [APPLICATION_PDF_VALUE])
+    fun getDiplomas(@PathVariable("competitionId") competitionId: Long): ResponseEntity<ByteArray> {
         val diplomas = competitionRankingService.createDiplomas(competitionId)
         return httpContentUtil.getContentAsPdf("diploma_$competitionId.pdf", diplomas)
     }
 
-    @GetMapping("/ranking/series/{id}")
-    fun getSeriesRanking(@PathVariable("id") seriesId: Long): ModelAndView {
+    @GetMapping("/ranking/series/{seriesId}")
+    fun getSeriesRanking(@PathVariable("seriesId") seriesId: Long): ModelAndView {
         val mav = ModelAndView("/series_ranking")
         mav.model["data"] = seriesRankingService.getSeriesRankingData(seriesId)
         return mav
     }
 
-    @GetMapping("/ranking/series/{id}/pdf", produces = [APPLICATION_PDF_VALUE])
-    fun getSeriesRankingAsPdf(@PathVariable("id") seriesId: Long): ResponseEntity<ByteArray> {
+    @GetMapping("/ranking/series/{seriesId}/pdf", produces = [APPLICATION_PDF_VALUE])
+    fun getSeriesRankingAsPdf(@PathVariable("seriesId") seriesId: Long): ResponseEntity<ByteArray> {
         val competitionRanking = seriesRankingService.createSeriesRanking(seriesId)
         return httpContentUtil.getContentAsPdf("series_$seriesId.pdf", competitionRanking)
     }
 
-    @GetMapping("/ranking/club/{id}/pdf", produces = [APPLICATION_PDF_VALUE])
-    fun getClubRankingAsPdf(@PathVariable("id") seriesId: Long): ResponseEntity<ByteArray> {
+    @GetMapping("/ranking/club/{seriesId}/pdf", produces = [APPLICATION_PDF_VALUE])
+    fun getClubRankingAsPdf(@PathVariable("seriesId") seriesId: Long): ResponseEntity<ByteArray> {
         val competitionRanking = clubRankingService.createClubRanking(seriesId)
         return httpContentUtil.getContentAsPdf("clubs_$seriesId.pdf", competitionRanking)
     }
