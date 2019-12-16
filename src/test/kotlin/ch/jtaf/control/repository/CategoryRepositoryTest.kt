dead7eb79f0d5fd547e8782e0b22d94065353090
Simon Martinelli <simon@72.services>
2019-12-16 15:02:33 +0100

    Version update

--- src/test/kotlin/ch/jtaf/control/repository/CategoryRepositoryTest.kt
+++ src/test/kotlin/ch/jtaf/control/repository/CategoryRepositoryTest.kt
@@ -1,15 +1,12 @@
 package ch.jtaf.control.repository
 
 import ch.jtaf.entity.Gender
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @DataJpaTest
 class CategoryRepositoryTest : AbstractRepositoryTest() {
 
--- src/test/kotlin/ch/jtaf/control/repository/ResultRepositoryTest.kt
+++ src/test/kotlin/ch/jtaf/control/repository/ResultRepositoryTest.kt
@@ -1,13 +1,10 @@
 package ch.jtaf.control.repository
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @DataJpaTest
 class ResultRepositoryTest : AbstractRepositoryTest() {
 
